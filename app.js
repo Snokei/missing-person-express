4c32564b00c8cd9a30501005767c04b1a0a0a0cd
Snokei <gmahor007@gmail.com>
2026-07-05 02:23:01 +0530

    allow cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 const notFound = require("./middleware/notFound");
 const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(routes);
 app.use(notFound);
