26d88cf15ed2782fd115c47aaee1b6abc6653d28
Snokei <gmahor007@gmail.com>
2026-07-04 16:14:37 +0530

    fixed

--- models/User.js
+++ models/User.js
@@ -52,16 +52,6 @@ const User = sequelize.define(
       allowNull: true,
     },
 
-    gov_card_type: {
-      type: DataTypes.ENUM("aadhaar", "pan", "voter"),
-      allowNull: true,
-    },
-
-    gov_card: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-
     is_active: {
       type: DataTypes.BOOLEAN,
       defaultValue: true,
