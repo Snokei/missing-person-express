ef83868fcdee087a211bd72c05ad532b6138929a
Snokei <gmahor007@gmail.com>
2026-07-05 05:42:25 +0530

    whitelist

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(routes);
 app.use(notFound);
